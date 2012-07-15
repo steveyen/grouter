7b4364b51f4bff88406643b66762baf47d762e48
Steve Yen <steve.yen@gmail.com>
2012-07-14 23:26:59 -0700

    added target param placeholder

--- main/grouter.go
+++ main/grouter.go
@@ -8,7 +8,13 @@ import (
 	"github.com/steveyen/grouter"
 )
 
-func MainServer(listen string, maxConns int) {
+func MainServer(listen string, maxConns int, target string, targetChanSize int) {
+	log.Printf("grouter")
+	log.Printf("  listen: %v", listen)
+	log.Printf("    maxConns: %v", maxConns)
+	log.Printf("  target: %v", target)
+	log.Printf("    targetChanSize: %v", targetChanSize)
+
 	ls, e := net.Listen("tcp", listen)
 	if e != nil {
 		log.Fatalf("error: could not listen on: %s; error: %s", listen, e)
@@ -16,8 +22,7 @@ func MainServer(listen string, maxConns int) {
 		defer ls.Close()
 		log.Printf("listening to: %s", listen)
 
-		memoryChanSize := 5
-		memoryChanRequest := make(chan grouter.Request, memoryChanSize)
+		memoryChanRequest := make(chan grouter.Request, targetChanSize)
 		go func() {
 			grouter.MemoryStorageRun(memoryChanRequest)
 		}()
@@ -26,11 +31,15 @@ func MainServer(listen string, maxConns int) {
 }
 
 func main() {
-	var listen *string = flag.String("listen", ":11300",
+	listen := flag.String("listen", ":11300",
 		"local address (<optional address>:port) to listen to")
-	var maxConns *int = flag.Int("max-conns", 3,
+	maxConns := flag.Int("max-conns", 3,
 		"max conns allowed from clients")
+	target := flag.String("target", "memory:",
+		"valid targets are memory:, memcached://HOST:PORT")
+	targetChanSize := flag.Int("target-chan-size", 5,
+		"target chan size")
 	flag.Parse()
-	MainServer(*listen, *maxConns)
+	MainServer(*listen, *maxConns, *target, *targetChanSize)
 }
 
