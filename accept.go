f305efe94ba2fe2be3c7726244bda4ea48d4a634
Steve Yen <steve.yen@gmail.com>
2012-07-15 21:57:10 -0700

    folded net.go into accept.go

--- accept.go
+++ accept.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"log"
 	"net"
+	"time"
 
 	"github.com/dustin/gomemcached"
 )
@@ -68,3 +69,24 @@ func AcceptConns(ls net.Listener, maxConns int, source Source, targetChan chan [
 	}
 }
 
+// Provides a capped, exponential-backoff retry loop around a dialer func.
+func Reconnect(spec string, dialer func(string) (interface{}, error)) interface{} {
+	sleep := 100 * time.Millisecond
+	for {
+		client, err := dialer(spec)
+		if err != nil {
+			if sleep > 2000 * time.Millisecond {
+				sleep = 2000 * time.Millisecond
+			}
+			log.Printf("warn: reconnect failed: %s;" +
+				" sleeping (ms): %d; err: %v",
+				spec, sleep / time.Millisecond, err)
+			time.Sleep(sleep)
+			sleep = sleep * 2
+		} else {
+			return client
+		}
+	}
+
+	return nil // Unreachable.
+}
