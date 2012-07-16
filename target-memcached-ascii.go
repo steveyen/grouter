340bf70bc75aedbd98a130d5c885df32f7848ecd
Steve Yen <steve.yen@gmail.com>
2012-07-15 17:48:09 -0700

    refactored out AsciiReadLines helper func()

--- target-memcached-ascii.go
+++ target-memcached-ascii.go
@@ -23,69 +23,88 @@ var MemcachedAsciiTargetHandlers = map[gomemcached.CommandCode]MemcachedAsciiTar
 		bw.Write(crnl)
 		bw.Flush()
 
-		hadValue := false
-
-		for {
-			line, isPrefix, err := br.ReadLine()
-			if err != nil {
-				return err
-			}
-			if isPrefix {
-				return fmt.Errorf("error: line is too long")
-			}
-
-			parts := strings.Split(string(line), " ")
-			if parts[0] == "VALUE" {
-				flg, err := strconv.Atoi(parts[2])
-				if err != nil {
-					return err
-				}
-
-				nval, err := strconv.Atoi(parts[3])
-				if err != nil {
-					return err
-				}
-
-				buf := make([]byte, nval + 2)
-				nbuf, err := io.ReadFull(br, buf)
-				if err != nil {
-					return err
-				}
-				if nbuf != nval + 2 {
-					return fmt.Errorf("error: nbuf mismatch: %d != %d", nbuf, nval + 2)
-				}
-				if !bytes.Equal(buf[nbuf - 2:], crnl) {
-					return fmt.Errorf("error: was expecting crlf")
-				}
-
-				extras := make([]byte, 4)
-				binary.BigEndian.PutUint32(extras, uint32(flg))
-
+		numValues, endParts, err := AsciiReadLines(br, req)
+		if err != nil {
+			return err
+		}
+		if endParts[0] == "END" {
+			if numValues <= 0 {
 				req.Res <-&gomemcached.MCResponse{
 					Opcode: req.Req.Opcode,
-					Status: gomemcached.SUCCESS,
+					Status: gomemcached.KEY_ENOENT,
 					Opaque: req.Req.Opaque,
-					Extras: extras,
-					Key: []byte(parts[1]),
-					Body: buf[:nval],
+					Key: req.Req.Key,
 				}
-
-				hadValue = true
-			} else {
-				if !hadValue {
-					req.Res <-&gomemcached.MCResponse{
-						Opcode: req.Req.Opcode,
-						Status: gomemcached.KEY_ENOENT,
-						Opaque: req.Req.Opaque,
-						Key: req.Req.Key,
-					}
-				}
-				return nil
+			}
+		} else {
+			req.Res <-&gomemcached.MCResponse{
+				Opcode: req.Req.Opcode,
+				Status: gomemcached.EINVAL,
+				Opaque: req.Req.Opaque,
+				Key: req.Req.Key,
 			}
 		}
-
-		return fmt.Errorf("error: unreachable was reached")
+		return nil
 	},
+}
+
+func AsciiReadLines(br *bufio.Reader, req Request) (int, []string, error) {
+	numValues := 0
+
+	for {
+		line, isPrefix, err := br.ReadLine()
+		if err != nil {
+			return numValues, nil, err
+		}
+		if isPrefix {
+			return numValues, nil, fmt.Errorf("error: line is too long")
+		}
+
+		parts := strings.Split(string(line), " ")
+		if parts[0] == "VALUE" {
+			flg, err := strconv.Atoi(parts[2])
+			if err != nil {
+				return numValues, parts, err
+			}
+
+			nval, err := strconv.Atoi(parts[3])
+			if err != nil {
+				return numValues, parts, err
+			}
+
+			buf := make([]byte, nval + 2)
+			nbuf, err := io.ReadFull(br, buf)
+			if err != nil {
+				return numValues, parts, err
+			}
+			if nbuf != nval + 2 {
+				err = fmt.Errorf("error: nbuf mismatch: %d != %d", nbuf, nval + 2)
+				return numValues, parts, err
+			}
+			if !bytes.Equal(buf[nbuf - 2:], crnl) {
+				err = fmt.Errorf("error: was expecting crlf")
+				return numValues, parts, err
+			}
+
+			extras := make([]byte, 4)
+			binary.BigEndian.PutUint32(extras, uint32(flg))
+
+			req.Res <-&gomemcached.MCResponse{
+				Opcode: req.Req.Opcode,
+				Status: gomemcached.SUCCESS,
+				Opaque: req.Req.Opaque,
+				Extras: extras,
+				Key: []byte(parts[1]),
+				Body: buf[:nval],
+			}
+
+			numValues++
+		} else {
+			return numValues, parts, nil
+		}
+	}
+
+	return numValues, nil, fmt.Errorf("error: unreachable was reached")
 }
 
 func MemcachedAsciiTargetRun(spec string, incoming chan Request) {
