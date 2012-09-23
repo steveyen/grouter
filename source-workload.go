f475b208243607907439ef876ec997052429626f
Steve Yen <steve.yen@gmail.com>
2012-09-22 19:04:56 -0700

    go fmt'ed

--- source-workload.go
+++ source-workload.go
@@ -46,8 +46,14 @@ func run(sourceSpec string, sourceMaxConns int, targetChan chan []Request,
 		if tot_workload_ops%report_every == 0 {
 			statsChan <- Stats{
 				Time: reqs_end,
-				Keys: []string {"tot_workload_ops", "tot_workload_ops_usecs"},
-				Vals: []int64 {int64(tot_workload_ops), int64(tot_workload_ops_nsecs / 1000)},
+				Keys: []string{
+					"tot_workload_ops",
+					"tot_workload_ops_usecs",
+				},
+				Vals: []int64{
+					int64(tot_workload_ops),
+					int64(tot_workload_ops_nsecs / 1000),
+				},
 			}
 			tot_workload_ops_nsecs = int64(0)
 			tot_workload_ops = 0
