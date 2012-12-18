abb3a32f68a8d9bbf7f667aa44eb149a598a4da7
Steve Yen <steve.yen@gmail.com>
2012-12-17 17:58:42 -0800

    lowercasing for hiding

--- grouter/grouter.go
+++ grouter/grouter.go
@@ -9,60 +9,60 @@ import (
 	"github.com/steveyen/grouter"
 )
 
-type EndPoint struct {
-	Usage string // Help string.
-	Descrip string
-	RunSource func(string, grouter.Params, grouter.Target, chan grouter.Stats)
-	StartTarget func(string, grouter.Params, chan grouter.Stats) grouter.Target
-	MaxConcurrency int // Some end-points have limited concurrency.
+type endPoint struct {
+	usage string // Help string.
+	descrip string
+	runSource func(string, grouter.Params, grouter.Target, chan grouter.Stats)
+	startTarget func(string, grouter.Params, chan grouter.Stats) grouter.Target
+	maxConcurrency int // Some end-points have limited concurrency.
 }
 
 // Available sources of requests.
-var Sources = map[string]EndPoint{
-	"memcached": EndPoint{
-		Usage: "memcached:LISTEN_INTERFACE:LISTEN_PORT",
-		Descrip: "memcached ascii source",
-		RunSource: grouter.MakeListenSourceFunc(&grouter.AsciiSource{}),
+var sources = map[string]endPoint{
+	"memcached": endPoint{
+		usage: "memcached:LISTEN_INTERFACE:LISTEN_PORT",
+		descrip: "memcached ascii source",
+		runSource: grouter.MakeListenSourceFunc(&grouter.AsciiSource{}),
 	},
-	"memcached-ascii": EndPoint{
-		Usage: "memcached-ascii:LISTEN_INTERFACE:LISTEN_PORT",
-		Descrip: "memcached ascii source",
-		RunSource: grouter.MakeListenSourceFunc(&grouter.AsciiSource{}),
+	"memcached-ascii": endPoint{
+		usage: "memcached-ascii:LISTEN_INTERFACE:LISTEN_PORT",
+		descrip: "memcached ascii source",
+		runSource: grouter.MakeListenSourceFunc(&grouter.AsciiSource{}),
 	},
-	"workload": EndPoint{
-		Usage: "workload",
-		Descrip: "a simple workload generator",
-		RunSource: grouter.WorkLoadRun,
+	"workload": endPoint{
+		usage: "workload",
+		descrip: "a simple workload generator",
+		runSource: grouter.WorkLoadRun,
 	},
 }
 
 // Available targets of requests.
-var Targets = map[string]EndPoint{
-	"http": EndPoint{
-		Usage: "http://COUCHBASE_HOST:COUCHBASE_PORT",
-		Descrip: "couchbase server as a target",
-        StartTarget: grouter.CouchbaseTargetStart,
+var targets = map[string]endPoint{
+	"http": endPoint{
+		usage: "http://COUCHBASE_HOST:COUCHBASE_PORT",
+		descrip: "couchbase server as a target",
+        startTarget: grouter.CouchbaseTargetStart,
 	},
-	"couchbase": EndPoint{
-		Usage: "couchbase://COUCHBASE_HOST:COUCHBASE_PORT",
-		Descrip: "couchbase server as a target",
-        StartTarget: grouter.CouchbaseTargetStart,
+	"couchbase": endPoint{
+		usage: "couchbase://COUCHBASE_HOST:COUCHBASE_PORT",
+		descrip: "couchbase server as a target",
+        startTarget: grouter.CouchbaseTargetStart,
 	},
-	"memcached-ascii": EndPoint{
-		Usage: "memcached-ascii:HOST:PORT",
-		Descrip: "memcached (ascii protocol) server as a target",
-		StartTarget: grouter.MemcachedAsciiTargetStart,
+	"memcached-ascii": endPoint{
+		usage: "memcached-ascii:HOST:PORT",
+		descrip: "memcached (ascii protocol) server as a target",
+		startTarget: grouter.MemcachedAsciiTargetStart,
 	},
-	"memcached-binary": EndPoint{
-		Usage: "memcached-binary:HOST:PORT",
-		Descrip: "memcached (binary protocol) server as a target",
-		StartTarget: grouter.MemcachedBinaryTargetStart,
+	"memcached-binary": endPoint{
+		usage: "memcached-binary:HOST:PORT",
+		descrip: "memcached (binary protocol) server as a target",
+		startTarget: grouter.MemcachedBinaryTargetStart,
 	},
-	"memory": EndPoint{
-		Usage: "memory",
-		Descrip: "simple in-memory hashtable target",
-		StartTarget: grouter.MemoryStorageStart,
-		MaxConcurrency: 1,
+	"memory": endPoint{
+		usage: "memory",
+		descrip: "simple in-memory hashtable target",
+		startTarget: grouter.MemoryStorageStart,
+		maxConcurrency: 1,
 	},
 }
 
@@ -70,14 +70,14 @@ func main() {
 	sourceSpec := flag.String("source", "memcached-ascii::11300",
         "source of requests\n" +
         "    as SOURCE_KIND[:MORE_PARAMS]\n" +
-        "    examples..." + EndPointExamples(Sources))
+        "    examples..." + endPointExamples(sources))
 	sourceMaxConns := flag.Int("source-max-conns", 100,
 		"max conns allowed into source")
 
 	targetSpec := flag.String("target", "memory",
         "target of requests\n" +
         "    as TARGET_KIND[:MORE_PARAMS]\n" +
-        "    examples..." + EndPointExamples(Targets))
+        "    examples..." + endPointExamples(targets))
 	targetChanSize := flag.Int("target-chan-size", 5,
 		"target chan size to control queuing")
 	targetConcurrency := flag.Int("target-concurrency", 4,
@@ -105,12 +105,12 @@ func main() {
 
 func MainStart(params grouter.Params) {
 	sourceKind := strings.Split(params.SourceSpec, ":")[0]
-	if sourceDef, ok := Sources[sourceKind]; ok {
+	if sourceDef, ok := sources[sourceKind]; ok {
 		targetKind := strings.Split(params.TargetSpec, ":")[0]
-		if targetDef, ok := Targets[targetKind]; ok {
-			if (targetDef.MaxConcurrency > 0 &&
-				targetDef.MaxConcurrency < params.TargetConcurrency) {
-				params.TargetConcurrency = targetDef.MaxConcurrency
+		if targetDef, ok := targets[targetKind]; ok {
+			if (targetDef.maxConcurrency > 0 &&
+				targetDef.maxConcurrency < params.TargetConcurrency) {
+				params.TargetConcurrency = targetDef.maxConcurrency
 				log.Printf("    target-concurrency clipped to: %v;" +
 					" due to limitations of target kind: %v",
 					params.TargetConcurrency, targetKind)
@@ -119,9 +119,9 @@ func MainStart(params grouter.Params) {
 			statsChan := grouter.StartStatsReporter(
 				params.SourceMaxConns + params.TargetConcurrency)
 
-			target := targetDef.StartTarget(params.TargetSpec, params, statsChan)
+			target := targetDef.startTarget(params.TargetSpec, params, statsChan)
 
-			sourceDef.RunSource(params.SourceSpec, params, target, statsChan)
+			sourceDef.runSource(params.SourceSpec, params, target, statsChan)
 		} else {
 			log.Fatalf("error: unknown target kind: %s", params.TargetSpec)
 		}
@@ -130,7 +130,7 @@ func MainStart(params grouter.Params) {
 	}
 }
 
-func EndPointExamples(m map[string]EndPoint) (rv string) {
+func endPointExamples(m map[string]endPoint) (rv string) {
 	mk := make([]string, len(m))
 	i := 0
     for k, _ := range m {
@@ -140,8 +140,8 @@ func EndPointExamples(m map[string]EndPoint) (rv string) {
     sort.Strings(mk)
 	rv = ""
 	for _, s := range mk {
-		rv = rv + "\n      " + m[s].Usage
-		rv = rv + "\n        -- " + m[s].Descrip
+		rv = rv + "\n      " + m[s].usage
+		rv = rv + "\n        -- " + m[s].descrip
 	}
 	return rv
 }
